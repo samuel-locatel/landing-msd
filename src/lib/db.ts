2e25847f40b98900924649318f7c186e09fceeba
Samuel Locatel <samuellocatel@gmail.com>
2026-04-05 22:48:27 -0300

    fix: use PrismaNeonHttp adapter and add prisma generate to build

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,10 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaNeonHttp } from '@prisma/adapter-neon'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 function createPrismaClient() {
-  const connectionString = process.env.DATABASE_URL!
-  const adapter = new PrismaNeon({ connectionString })
+  const adapter = new PrismaNeonHttp(process.env.DATABASE_URL!, {})
   return new PrismaClient({ adapter })
 }
 
